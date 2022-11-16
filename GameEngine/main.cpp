7f0ae976bbf3023c52e7316b0ea2d424cb979e82
Ben Crabbe <ben.crabbe@codaoctopus.com>
2022-11-16 16:39:01 +0000

    clean up main

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -1,25 +1,7 @@
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
-#include <iostream>
-
 #include "Application.h"
 
 int main()
 {
 	Application app{ "Vulkan", 800, 800 };
-
-	try
-	{
-		app.run();
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS;
+	app.run();
 }
